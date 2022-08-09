12290682152f8f4bec2fd9be4538a51676b53f4f
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-09 00:50:54 -0400

    POST request displays proper responses

--- api/server.js
+++ api/server.js
@@ -18,8 +18,31 @@ server.get('/api/users', (req, res) => {
     })
     .catch(() => {
         res.status(500).json({ message: "The users information could not be retrieved" })
-    })
+    });
 })
 
+server.post('/api/users', (req, res) => {
+    Users.insert(req.body)
+    .then(result => {
+        res.status(201).json(result);
+    })
+    .catch(() => {
+        res.status(500).json({message: "Please provide name and bio for the user" })
+    });
+})
+
+server.delete(('/api/users/:id', (req, res) => {
+    Users.remove(req.params.id)
+        .then(result => {
+            if(result == null) {
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+            } else {
+                res.json(result);
+            }
+        })
+        .catch(()=> {
+            res.status(500).json({message: "The user could not be removed"})
+        });
+}))
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
