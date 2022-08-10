11ba7cccabb2c3a485d49bfda05ec3090024fd8f
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-10 10:49:37 -0400

    all tasks completed

--- api/server.js
+++ api/server.js
@@ -15,10 +15,10 @@ server.get('/', (req, res) => {
 //`findById` Takes an `id` 
 //and resolves to the user with that id (or null if the id does not exist).
 
-server.get(('/api/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     Users.findById(req.params.id)
         .then(result => {
-            if(result == null) {
+            if(!result) {
                 res.status(404).json({ message: "The user with the specified ID does not exist" })
             } else {
                 res.status(200).json(result)
@@ -27,7 +27,7 @@ server.get(('/api/users/:id', (req, res) => {
          .catch(() => {
             res.status(500).json({ message: "The user information could not be retrieved" })
      });
-}));
+});
 
 
 server.get('/api/users', (req, res) => {
@@ -43,7 +43,7 @@ server.get('/api/users', (req, res) => {
 server.post('/api/users', (req, res) => {
     Users.insert(req.body)
     .then(result => {
-        if(result == null) {
+        if(!result.name || !result.bio) {
             res.status(400).json({ message: "Please provide name and bio for the user" })
         } else {
             res.status(201).json(result);
@@ -55,18 +55,46 @@ server.post('/api/users', (req, res) => {
     });
 })
 
-server.delete(('/api/users/:id', (req, res) => {
-    Users.remove(req.params.id)
-        .then(result => {
-            if(result == null) {
-                res.status(404).json({message: "The user with the specified ID does not exist"})
-            } else {
-                res.json(result);
-            }
-        })
-        .catch(()=> {
-            res.status(500).json({message: "The user could not be removed"})
-        });
-}))
+server.delete('/api/users/:id', (req, res) => {
+    let user
+    Users.findById(req.params.id)
+    .then((userToDelete) => {
+        if( !userToDelete ) {
+            console.log(userToDelete)
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        } else {
+            user = userToDelete
+            //delete my user
+            Users.remove(req.params.id)
+            return;
+        }
+    })
+    .then(() => {
+        //return user
+        res.status(200).json(user)
+    })
+    .catch((err) => {
+        res.status(500).json({ message: "The user could not be removed"})
+    })
+
+    server.put("/api/users/:id", (req, res) => {
+        Users.update(req.params.id, req.body)
+            .then(result => {
+                if(!result){
+                   res.status(404).json({message: "The user with the specified ID does not exist"}); 
+                } 
+                else if(!req.body.name || !req.body.bio) {   
+                    res.status(400).json({message: "Please provide name and bio for the user"})
+                    
+                }  else {
+                    res.status(200).json(result)
+                }
+            })
+            .catch(() => {
+                res.status(500).json({message: "The user information could not be modified"})
+            })
+    })
+   
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
